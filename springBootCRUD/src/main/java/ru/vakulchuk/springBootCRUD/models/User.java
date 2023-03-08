164aaca90d03dc95b335f06ddb713baf9c4675ad
Артур Вакульчук <115490870+arthur4ik@users.noreply.github.com>
2023-03-09 02:15:37 +0300

    Add files via upload

---
+++ springBootCRUD/src/main/java/ru/vakulchuk/springBootCRUD/models/User.java
@@ -0,0 +1,81 @@
+package ru.vakulchuk.springBootCRUD.models;
+
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotEmpty;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+
+    @Column(name = "name")
+    @NotEmpty(message = "Please enter a valid name")
+    private String name;
+
+
+    @Column(name = "surname")
+    @NotEmpty(message = "Please enter a valid surname")
+    private String surname;
+
+
+    @Column(name = "email")
+    @NotEmpty(message = "Please enter a valid email")
+    private String email;
+
+    public User() {
+
+    }
+
+    public User(String name, String surname, String email) {
+        this.name = name;
+        this.surname = surname;
+        this.email = email;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", surname='" + surname + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+}
